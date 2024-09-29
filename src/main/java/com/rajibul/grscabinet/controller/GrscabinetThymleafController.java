bb6c1eaabff1c19948aa2dfec097870e64a16026
rajibul5603 <russelice08@gmail.com>
2024-09-29 13:42:28 +0600

    thymeleaf added

---
+++ src/main/java/com/rajibul/grscabinet/controller/GrscabinetThymleafController.java
@@ -0,0 +1,41 @@
+package com.rajibul.grscabinet.controller;
+
+
+import com.rajibul.grscabinet.service.GrscabinetService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/thymeleaf/complains")
+@Slf4j
+public class GrscabinetThymleafController {
+
+    @Autowired
+    private GrscabinetService grscabinetService;
+
+    @GetMapping
+    public String listGrscabinetComplain(Model model)
+    {
+        System.out.println("Entered into  complain url");
+        model.addAttribute("complainlist", grscabinetService.getAllGrsComplain());
+        model.addAttribute("sectionHeader", "Complain List");
+        return "thymeleaf/complains/index";
+    }
+
+    @GetMapping("/new")
+    public String createGrscabinetComplain(Model model) {
+
+        System.out.println("Entered into add complain url");
+        model.addAttribute("sectionHeader", "Add Complain");
+        return "thymeleaf/complains/add";
+    }
+
+
+
+
+}
